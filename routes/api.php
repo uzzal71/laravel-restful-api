ea2888a9146d4815862f4ba4ae7ac736908fc8cc
uzzal71 <uzzalroy.acm@gmail.com>
2022-06-10 18:50:53 +0600

    author model, api route, controller crated:

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\PetitionController;
 
 /*
@@ -19,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::resource('/authors', AuthorController::class)->only(['index', 'show']);
 Route::apiResource('/petitions', PetitionController::class);
